4ee94997e2b97c8749eca89003225024eb77dab8
AbhijeetGidde <166475209+AbhijeetGidde@users.noreply.github.com>
2025-01-12 23:55:51 +0530

    First host

---
+++ index.js
@@ -0,0 +1,15 @@
+function showpop(popId) {
+    document.getElementById(popId).style.display = 'flex';
+  }
+
+  function closepop() {
+    document.getElementById(popId).style.display = 'none';
+  }
+
+
+function onhover(home)
+  {home.style.backgroundColor='rgba(255, 255, 0, 0.639)'
+home.style.borderRadius='10px'
+}
+
+function onout(home){{home.style.backgroundColor='transparent'}}
